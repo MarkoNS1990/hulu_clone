cd1eef0c7f6125bed31b0219f60ed677012debfd
MarkoNS1990 <marczecevic@gmail.com>
2021-08-16 10:36:23 +0200

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+function Header() {
+  return (
+    <header className="">
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+      </div>
+      <Image
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+        className="object-contain"
+      />
+    </header>
+  );
+}
+
+export default Header;
